814cea11f83fffc9b94e4e2e681e276b02c63a8b
Templars <Templars@TEMPLARS-PC>
2020-04-22 13:49:40 +0800

    Update models DB

--- flask_blog/models.py
+++ flask_blog/models.py
@@ -11,6 +11,16 @@ class Shipping(db.Model):
     def __repr__(self):
         return f"Shipping('id: {self.id}, method: {self.method}, weight: {self.weight}, price_per_pound: {self.price_per_pound}, flat_charges: {self.flat_charges}')"
 
+class Shipper(db.Model):
+    shipper_id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(40), nullable=False)
+    phone = db.Column(db.String(40), nullable=False)
+
+    orders = db.relationship('Orders', backref='shipped', lazy=True)
+    
+    def __repr__(self):
+        return f"Shipper('shipper_id (PK): {self.shipper_id}, company_name: {self.company_name}, phone: {self.phone}')"
+
 class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(40), nullable=False)
@@ -22,60 +32,55 @@ class Customer(db.Model):
     country = db.Column(db.String(40), nullable=False)
     phone = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(40), nullable=False)
+
     orders = db.relationship('Orders', backref='buy', lazy=True)
 
     def __repr__(self):
-        return f"Customer('customer_id: {self.customer_id}, first_name: {self.first_name}, last_name: {self.last_name}, phone: {self.phone}, email: {self.email}')"
+        return f"Customer('customer_id(PK): {self.customer_id}, first_name: {self.first_name}, last_name: {self.last_name}, phone: {self.phone}, email: {self.email}')"
 
 class Orders(db.Model):
     order_id = db.Column(db.Integer, primary_key=True)
     order_number = db.Column(db.Integer, nullable=False)
     customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), nullable=False)
     shipper_id = db.Column(db.Integer, db.ForeignKey('shipper.shipper_id'), nullable=False)
+
     order_details = db.relationship('Details', backref='has', lazy=True)
     customer = db.relationship('Customer', backref='ordered', lazy=True)
     shipper = db.relationship('Shipper', backref='managed', lazy=True)
     
     def __repr__(self):
-        return f"Orders('order_id: {self.order_id}, order_number: {self.order_number},)"
+        return f"Orders('order_id(PK): {self.order_id}, customer_id(FK): {self.customer_id}, shipper_id(FK):{self.shipper_id},  order_number: {self.order_number},)"
 
-class Details(db.Model):
+class OrderDetails(db.Model):
     order_details_id = db.Column(db.Integer, primary_key=True)
     quantity = db.Column(db.Integer, nullable=False, default=1)
     price = db.Column(db.Float, nullable=False)  
     total = db.Column(db.Float, nullable=False)
     order_id = db.Column(db.Integer, db.ForeignKey('orders.order_id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'), nullable=False)
+
     order = db.relationship('Orders', backref='fromorder', lazy=True)
-    product = db.relationship('Products', backref='has', lazy=True)
+    product = db.relationship('Products', backref='contains', lazy=True)
     
     def __repr__(self):
-        return f"Details('order_details_id: {self.order_details_id}, quantity: {self.quantity}, price: {self.price}, total: {self.total}')"
+        return f"OrderDetails('order_details_id(PK): {self.order_details_id}, product_id(FK): {self.product_id}, order_id(FK): {self.order_id}, quantity: {self.quantity}, price: {self.price}, total: {self.total}')"
 
 class Products(db.Model):
     product_id = db.Column(db.Integer, primary_key=True)
     product_name = db.Column(db.String(40), nullable=False)
     product_description = db.Column(db.String(100), nullable=False)
     unit_price = db.Column(db.Float, nullable=False)
-    order_details_id = db.Column(db.Integer, db.ForeignKey('details.order_details_id'), nullable=True)
     category_id = db.Column(db.Integer, db.ForeignKey('category.category_id'), nullable=False)
     
     def __repr__(self):
-        return f"Products('product_id: {self.product_id}, product_name: {self.product_name}, product_description: {self.product_description}, unit_price: {self.unit_price}')"
+        return f"Products('product_id(PK): {self.product_id}, category_id(FK): {self.category_id}, product_name: {self.product_name}, product_description: {self.product_description}, unit_price: {self.unit_price}')"
 
 class Category(db.Model):
     category_id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(40), nullable=False)
     category_description = db.Column(db.String(100), nullable=False)
+
     products = db.relationship('Products', backref='contains', lazy=True)
     
     def __repr__(self):
-        return f"Category('category_id: {self.category_id}, category_name: {self.category_name}, category_description: {self.category_description}')"
-
-class Shipper(db.Model):
-    shipper_id = db.Column(db.Integer, primary_key=True)
-    company_name = db.Column(db.String(40), nullable=False)
-    phone = db.Column(db.String(40), nullable=False)
-    orders = db.relationship('Orders', backref='shipped', lazy=True)
-    
-    def __repr__(self):
-        return f"Shipper('shipper_id: {self.shipper_id}, company_name: {self.company_name}, phone: {self.phone}')"
+        return f"Category('category_id(PK): {self.category_id}, category_name: {self.category_name}, category_description: {self.category_description}')"
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask_blog import app, db
 from flask_blog.forms import ShippingForm
-from flask_blog.models import Shipping, Customer, Orders, Details, Products, Category, Shipper
+from flask_blog.models import Shipping, Customer, Orders, OrderDetails, Products, Category, Shipper
 from flask_blog.functions import compare_shipping
 
 @app.route('/')
