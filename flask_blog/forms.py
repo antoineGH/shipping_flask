db6afff95010773b28b68b33793ca7a361157a23
Templars <Templars@TEMPLARS-PC>
2020-04-23 10:48:53 +0800

    Categories management interface

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, FloatField, HiddenField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange
-from flask_blog.models import Shipper
+from flask_blog.models import Shipper, Category
 
 class ShippingForm(FlaskForm):
     weight = FloatField('Package Weight', validators=[DataRequired(), NumberRange(min=0, max=100)], render_kw={"placeholder": "Enter package weight"})
@@ -10,7 +10,7 @@ class ShippingForm(FlaskForm):
 class ShipperForm(FlaskForm):
     company_name = StringField('Company Name', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder": "Company Name"})
     phone = StringField('Phone Number', validators=[DataRequired(), Length(min=10, max=15)], render_kw={"placeholder": "Phone Number"})
-    submit = SubmitField('Submit')
+    submit = SubmitField('Add')
 
     def validate_company_name(self, company_name):
         company_name = Shipper.query.filter_by(company_name=company_name.data).first()
@@ -30,3 +30,27 @@ class DeleteShipperForm(FlaskForm):
         shipper_delete_id = Shipper.query.filter_by(shipper_id=shipper_delete_id.data).first()
         if not shipper_delete_id:
             raise ValidationError('Please select an existing Shipper ID.')
+
+class CategoryForm(FlaskForm):
+    category_name = StringField('Category Name', validators=[DataRequired(), Length(min=2, max=20)], render_kw={"placeholder":"Category Name"})
+    category_description = StringField('Category Description', validators=[DataRequired(), Length(min=2, max=30)], render_kw={"placeholder":"Category Description"})
+    submit = SubmitField('Add')
+
+    def validate_category_name(self, category_name):
+        category_name = Category.query.filter_by(category_name=category_name.data).first()
+        if category_name:
+            raise ValidationError('That category name already exists. Please choose a different one.')
+
+    def validate_category_name(self, category_description):
+        category_description = Category.query.filter_by(category_description=category_description.data).first()
+        if category_description:
+            raise ValidationError('That category description already exists. Please choose a different one.')
+
+class DeleteCategoryForm(FlaskForm):
+    category_delete_id = IntegerField('Delete Category ID', validators=[DataRequired(), NumberRange(min=0, max=1000)], render_kw={"placeholder": "Category ID "})
+    delete = SubmitField('Delete')
+
+    def validate_shipper_delete_id(self, category_delete_id):
+        category_delete_id = Category.query.filter_by(categorye_id=category_delete_id.data).first()
+        if not category_delete_id:
+            raise ValidationError('Please select an existing Category ID.')
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask_blog import app, db
-from flask_blog.forms import ShippingForm, ShipperForm, DeleteShipperForm
+from flask_blog.forms import ShippingForm, ShipperForm, DeleteShipperForm, CategoryForm, DeleteCategoryForm
 from flask_blog.models import Shipping, Customer, Orders, OrderDetails, Products, Category, Shipper
 from flask_blog.functions import compare_shipping
 
@@ -16,26 +16,47 @@ def shipping():
     shipping = compare_shipping(form_shipping.weight.data)
     return render_template('shipping.html', title='Python', form_shipping=form_shipping, shipping=shipping, shipping_prices=shipping_prices)
 
-@app.route('/manage_shippers', methods=['GET', 'POST'])
-def manage_shippers():
+@app.route('/shippers', methods=['GET', 'POST'])
+def shippers():
     shipper = Shipper.query.all()
-    
     form_shipper = ShipperForm()
+    form_delete_shipper = DeleteShipperForm()
+
     if form_shipper.validate_on_submit():
         shipper = Shipper(company_name=form_shipper.company_name.data, phone=form_shipper.phone.data)
         db.session.add(shipper)
         db.session.commit()
         flash(f'You have successfully created {shipper.company_name}', 'success')
-        return redirect(url_for('manage_shippers'))
+        return redirect(url_for('shippers'))
 
-    form_delete_shipper = DeleteShipperForm()
     if form_delete_shipper.validate_on_submit(): 
         todelete = Shipper.query.filter_by(shipper_id=form_delete_shipper.shipper_delete_id.data).first()
         Shipper.query.filter_by(shipper_id=todelete.shipper_id).delete()   
         db.session.commit()
         flash(f'You have successfully delete {todelete.company_name}', 'success')
-        return redirect(url_for('manage_shippers'))
+        return redirect(url_for('shippers'))
     
-    return render_template('manage_shippers.html', title='Manage Shippers', form_shipper=form_shipper, shipper=shipper, form_delete_shipper=form_delete_shipper)
+    return render_template('shippers.html', title='Shippers', form_shipper=form_shipper, shipper=shipper, form_delete_shipper=form_delete_shipper)
 
+@app.route('/category', methods=['GET', 'POST'])
+def category():
+    category = Category.query.all()
+    form_category = CategoryForm()
+    form_delete_category = DeleteCategoryForm()
+
+    if form_category.validate_on_submit():
+        category = Category(category_name=form_category.category_name.data, category_description=form_category.category_description.data)
+        db.session.add(category)
+        db.session.commit()
+        flash(f'You have successfully created {category.category_name}', 'success')
+        return redirect(url_for('category'))
+
+    if form_delete_category.validate_on_submit(): 
+        todelete = Category.query.filter_by(category_id=form_delete_category.category_delete_id.data).first()
+        Category.query.filter_by(category_id=todelete.category_id).delete()   
+        db.session.commit()
+        flash(f'You have successfully delete {todelete.category_name}', 'success')
+        return redirect(url_for('category'))
+    
+    return render_template('category.html', title='Categories', form_category=form_category, category=category, form_delete_category=form_delete_category)
 
