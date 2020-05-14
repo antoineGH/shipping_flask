4717b205d099abe6b17f95f5a3f3b4591fea1199
Templars <Templars@TEMPLARS-PC>
2020-05-14 14:53:04 +0800

    Update Cart Functionnalities / Quantity / Total

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -117,7 +117,13 @@ class DeleteAccountForm(FlaskForm):
 class AddOrderForm(FlaskForm):
     quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=0, max=10)], render_kw={"placeholder": "1"})
     product_id = HiddenField('Product_ID')
+    order_details_id = HiddenField('Order Details ID')
     submit = SubmitField('Add to cart')
+
+class UpdateOrderForm(FlaskForm):
+    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=0, max=10)])
+    order_details_id = HiddenField('Order Details ID')
+    update = SubmitField('Update')
 
 class CreateOrderForm(FlaskForm):
     shipper_choice = get_shipper_choice()
@@ -128,6 +134,13 @@ class CreateOrderForm(FlaskForm):
         super(CreateOrderForm, self).__init__(*args, **kwargs)
         self.shipper_id.choices = get_shipper_choice()
 
+class DeleteOrderForm(FlaskForm):
+    order_details_id =  HiddenField('Delete Product ID')
+    delete = SubmitField('Remove')
+
 class GenInvoiceForm(FlaskForm):
     order_number = HiddenField('Order Number')
     submit = SubmitField('Order')
+
+class FilterForm(FlaskForm):
+    filter = HiddenField('Filter')
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -4,7 +4,7 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, make_response, abort
 import pdfkit
 from flask_blog import app, db, bcrypt
-from flask_blog.forms import ShippingForm, ShipperForm, DeleteShipperForm, CategoryForm, DeleteCategoryForm, ProductForm, DeleteProductForm, RegistrationForm, LoginForm, UpdateAccountForm, DeleteAccountForm, AddOrderForm, CreateOrderForm, GenInvoiceForm
+from flask_blog.forms import ShippingForm, ShipperForm, DeleteShipperForm, CategoryForm, DeleteCategoryForm, ProductForm, DeleteProductForm, RegistrationForm, LoginForm, UpdateAccountForm, DeleteAccountForm, AddOrderForm, CreateOrderForm, GenInvoiceForm, FilterForm, DeleteOrderForm, UpdateOrderForm
 from flask_blog.models import Shipping, User, Orders, OrderDetails, Products, Category, Shipper
 from flask_blog.functions import compare_shipping, get_category_choice, save_picture, save_picture_product, gen_order_number, calc_total_user, calc_total_item
 from flask_login import login_user, current_user, logout_user, login_required
@@ -176,32 +176,53 @@ def account():
 @app.route('/shop', methods=['GET','POST'])
 def shop():
     page = request.args.get('page', 1, type=int)
-    products = Products.query.paginate(per_page=8, page=page)
+    filter_str = request.args.get('filter_str', None, type=str)
     form = AddOrderForm()
-
+    filterform = FilterForm()
+    
     if request.method == 'GET':
         form.quantity.data = 1    
 
     if current_user.is_authenticated:
-        if 'asc' in request.form:
+        if filterform.filter.data == 'asc' :
+            print('asc form')
             products = Products.query.order_by(Products.unit_price.asc()).paginate(per_page=8, page=page)
-            return render_template('shop.html', title='Shop', products=products, form=form )
-            print("asc")
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='asc', filterform=filterform )
 
-        if 'desc' in request.form:
+        elif filterform.filter.data == 'desc':
+            print('desc form')
             products = Products.query.order_by(Products.unit_price.desc()).paginate(per_page=8, page=page)
-            return render_template('shop.html', title='Shop', products=products, form=form)
-            print("desc")
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='desc', filterform=filterform)
 
-        if 'az' in request.form:
+        elif filterform.filter.data == 'az':
+            print('az form')
             products = Products.query.order_by(Products.product_name.asc()).paginate(per_page=8, page=page)
-            return render_template('shop.html', title='Shop', products=products, form=form )
-            print("asc")
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='az', filterform=filterform )
 
-        if 'za' in request.form:
+        elif filterform.filter.data == 'za':
+            print('za form')
             products = Products.query.order_by(Products.product_name.desc()).paginate(per_page=8, page=page)
-            return render_template('shop.html', title='Shop', products=products, form=form)
-            print("desc")
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='za', filterform=filterform)
+
+        elif filter_str == 'asc':
+            print('asc not form')
+            products = Products.query.order_by(Products.unit_price.asc()).paginate(per_page=8, page=page)
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='asc', filterform=filterform )
+
+        elif filter_str == 'desc':
+            print('desc not form')
+            products = Products.query.order_by(Products.unit_price.desc()).paginate(per_page=8, page=page)
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='desc', filterform=filterform)
+
+        elif filter_str == 'az':
+            print('az not form')
+            products = Products.query.order_by(Products.product_name.asc()).paginate(per_page=8, page=page)
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='az', filterform=filterform )
+
+        elif filter_str == 'za':
+            print('za not form')
+            products = Products.query.order_by(Products.product_name.desc()).paginate(per_page=8, page=page)
+            return render_template('shop.html', title='Shop', products=products, form=form, filter_str='za', filterform=filterform)
 
         if form.validate_on_submit():
             product = Products.query.filter_by(product_id=form.product_id.data).first()
@@ -221,22 +242,25 @@ def shop():
 
             db.session.add(orderdetails)
             db.session.commit()
+            flash('You\'ve just added {} to your cart'.format(product.product_name), 'success')
             return redirect(url_for('shop'))
     
-    elif form.validate_on_submit() or 'asc' in request.form or 'desc' in request.form or 'az' in request.form or 'za' in request.form:
+    elif form.validate_on_submit() or filterform.validate_on_submit():
         flash('Please Log In to Add in your Cart.', 'warning')
         return redirect(url_for('login'))
+
+    products = Products.query.paginate(per_page=8, page=page)
+
     
-    return render_template('shop.html', title='Shop', products=products, form=form)
+    return render_template('shop.html', title='Shop', products=products, form=form, filter_str=filter_str, filterform=filterform)
 
 @app.route('/cart', methods=['GET','POST'])
 def cart():
     form = CreateOrderForm()
+    form_delete= DeleteOrderForm()
+    form_update= UpdateOrderForm()
     orderuser = OrderDetails.query.filter_by(user_id=current_user.user_id).all()
     order_details = OrderDetails.query.filter_by(user_id=current_user.user_id).all()
-
-    total_item = calc_total_item(orderuser)
-    total_user = calc_total_user(orderuser)
 
     if form.validate_on_submit():
         if current_user.is_authenticated:
@@ -246,6 +270,7 @@ def cart():
 
             for order_detail in order_details:
                 order_detail.order_id = order.order_id
+                order_detail.quantity = form_update.quantity.data
                 order_detail.user_id = 9999
             db.session.commit()
             return redirect(url_for('confirm'))
@@ -254,7 +279,48 @@ def cart():
             flash('Please Log In to Add to Cart.', 'warning')
             return redirect(url_for('login'))
 
-    return render_template('cart.html', title='Cart', form=form, order_details=order_details, total_user=total_user, total_item=total_item)
+    if form_update.update.data: 
+        if request.form['update'] == 'plus':
+            to_update = OrderDetails.query.filter_by(user_id=current_user.user_id).filter_by(order_details_id=form_update.order_details_id.data).first()
+            to_update.quantity = sum([to_update.quantity], start=1)
+            to_update.total = 0
+            to_update.total += (to_update.quantity * to_update.price)
+            db.session.commit()
+            total_item = calc_total_item(orderuser)
+            total_user = calc_total_user(orderuser)
+            return redirect(url_for('cart'))
+
+        elif request.form['update'] == 'minus':
+            to_update = OrderDetails.query.filter_by(user_id=current_user.user_id).filter_by(order_details_id=form_update.order_details_id.data).first()
+            to_update.quantity = sum([to_update.quantity], start=-1)
+            to_update.total = 0
+            to_update.total += (to_update.quantity * to_update.price)
+            
+            if to_update.total == 0:
+                to_delete = OrderDetails.query.filter_by(user_id = current_user.user_id).filter_by(order_details_id=to_update.order_details_id).all()
+
+                for delete in to_delete:
+                    OrderDetails.query.filter_by(order_details_id=delete.order_details_id).delete()
+
+            db.session.commit()
+
+            return redirect(url_for('cart'))
+        else:
+            pass
+            
+
+    if form_delete.delete.data and form_delete.validate_on_submit(): 
+            todelete = OrderDetails.query.filter_by(user_id=current_user.user_id).filter_by(order_details_id=form_delete.order_details_id.data).all()
+            for delete in todelete:
+                OrderDetails.query.filter_by(order_details_id=delete.order_details_id).delete()   
+            db.session.commit()
+            flash('Successfully deleted from your cart', 'success')
+            return redirect(url_for('cart'))
+
+    total_item = calc_total_item(orderuser)
+    total_user = calc_total_user(orderuser)
+
+    return render_template('cart.html', title='Cart', form=form, form_delete=form_delete, order_details=order_details, total_user=total_user, total_item=total_item, form_update=form_update)
 
 @app.route('/logout')
 def logout():
@@ -399,5 +465,3 @@ def shop_category(category_id):
         return redirect(url_for('login'))
     
     return render_template('shop_category.html', title='Shop - '+ category_name, products=products, form=form, category_name=category_name)
-
-
