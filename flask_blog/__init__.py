a9c29e9e6c6e884043cfa876e394f6361b55e3dd
Templars <Templars@TEMPLARS-PC>
2020-05-18 19:17:18 +0800

    Mail creditentials OS environ

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -15,8 +16,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.mail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'templars69@mail.com'
-app.config['MAIL_PASSWORD'] = 'J6JENUx2ttZK'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 mail = Mail(app)
 
 from flask_blog import routes
